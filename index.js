604fab3b7395cc13e3497189c823ff62d759680f
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-03-20 15:30:29 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 20
+let num4 = 16
+const num = [5, 10, 15, 20]
+const multiply = num1 * num2 
+const random = Math.ceil(Math.random() *10)
+const mod = num3 % num4
+const max = Math.max(5, 10, 15, 20)
